3462df1a4e431db1479ca4b976904e69280dac8f
Ethereummom <103083686+Ethereummom@users.noreply.github.com>
2023-04-16 00:07:28 +0900

    test02

--- GodSaeng/src/main/java/com/planner/godsaeng/controller/ChallengeController.java
+++ GodSaeng/src/main/java/com/planner/godsaeng/controller/ChallengeController.java
@@ -1,7 +1,18 @@
 package com.planner.godsaeng.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/challenge")
 public class ChallengeController {
 	
+	
+	
+	
 	//황주바보
+	//황주바보
+	
+	
 
 }
