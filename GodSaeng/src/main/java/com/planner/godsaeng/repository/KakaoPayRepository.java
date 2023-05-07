65bfe81b38df8a0563d09f52e3f2a3570f34d9eb
CITYPOPER <103362619+CITYPOPER@users.noreply.github.com>
2023-05-07 14:51:44 +0900

    결재내역 검색 추가

--- GodSaeng/src/main/java/com/planner/godsaeng/repository/KakaoPayRepository.java
+++ GodSaeng/src/main/java/com/planner/godsaeng/repository/KakaoPayRepository.java
@@ -1,9 +1,11 @@
 package com.planner.godsaeng.repository;
 
+import java.util.List;
 import java.util.Optional;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.planner.godsaeng.entity.Payment;
@@ -11,6 +13,11 @@ import com.planner.godsaeng.entity.Payment;
 @Repository
 public interface KakaoPayRepository extends JpaRepository<Payment, Long> {
 //	Optional<User> findByUser(User user);
-//	
+	
 //	Optional<Payment> findByUid(String uid);
+	
+	@Query(value = "SELECT * FROM godsaeng_payment WHERE uid = :uid", nativeQuery = true)
+	List<Payment> findPaymentByUid(@Param("uid") String uid);
+	
+//	List<Payment> findByUserUid(String userUid);
 }
