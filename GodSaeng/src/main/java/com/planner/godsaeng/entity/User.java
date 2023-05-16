b5ba271f8d3fa9d5008d1f656c8f11ba6356834b
Ethereummom <103083686+Ethereummom@users.noreply.github.com>
2023-05-16 16:58:12 +0900

    Update User.java

--- GodSaeng/src/main/java/com/planner/godsaeng/entity/User.java
+++ GodSaeng/src/main/java/com/planner/godsaeng/entity/User.java
@@ -1,9 +1,15 @@
 package com.planner.godsaeng.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CascadeType;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,19 +28,19 @@ import lombok.ToString;
 public class User {
    
    @Id
-   @Column(length=30, nullable=false)
+   @Column(length=100, nullable=false)
    private String uid;
    
    @Column(length=20, nullable=false)
    private String unickname;
    
-   @Column(length=20, nullable=false)
+   @Column(length=100, nullable=true)
    private String uzepid;
-   
+
    @Column(length=20, columnDefinition = "int default 0")
    private int udeposit;
    
-   @Column(length=20, nullable=false)
+   @Column(length=20, columnDefinition = "VARCHAR(20) DEFAULT ''")
    private String ugrade;
    
    @Column(length=20, columnDefinition = "int default 0")
@@ -43,7 +49,20 @@ public class User {
    @Column(length=100, nullable=false)
    private String ucontent;
    
-   @Column(length=20, nullable=false)
+   @Column(length=20, nullable=true)
    private String usuccessedchallenge;
    
+   @Column(nullable= false, columnDefinition = "int default 0")
+   private int uverifiedornot;
+   
+   @OneToMany(mappedBy = "user", cascade = javax.persistence.CascadeType.ALL, orphanRemoval = true)
+   private List<ChallengeParticipate> challengeParticipateList = new ArrayList<>();
+   
+   @OneToMany(mappedBy = "user", cascade = javax.persistence.CascadeType.ALL, orphanRemoval = true)
+   private List<Plan> plans;
+   
+   @OneToMany(mappedBy = "user", cascade = javax.persistence.CascadeType.ALL)
+   private List<Payment> payments;
+
 }
+   
