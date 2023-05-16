977a30440f470b83f98e517418e3926e711a4eea
Ethereummom <103083686+Ethereummom@users.noreply.github.com>
2023-05-16 16:57:12 +0900

    Update UserService.java

--- GodSaeng/src/main/java/com/planner/godsaeng/service/UserService.java
+++ GodSaeng/src/main/java/com/planner/godsaeng/service/UserService.java
@@ -1,35 +1,39 @@
 package com.planner.godsaeng.service;
 
 import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.planner.godsaeng.dto.UserDTO;
+import com.planner.godsaeng.dto.ZepIdVerifyDTO;
 import com.planner.godsaeng.entity.User;
 import com.planner.godsaeng.repository.UserRepository;
 
 @Service
 public class UserService {
-   
+
    @Autowired
    UserRepository userRepository;
-   
+
    User user = null;
-   
+
    //유저 회원가입(INSERT)
    public boolean InsertUser(UserDTO u) {
+	  Random r = new Random();
+	  int randomnumber = r.nextInt(100000);
       user = User.builder()
             .uid(u.getU_id())
             .unickname(u.getU_nickname())
-            .uzepid(u.getU_zepid())
+            .uzepid(u.getU_id() + randomnumber)
             .udeposit(u.getU_deposit())
             .ugrade(u.getU_grade())
             .ulevel(u.getU_level())
             .ucontent(u.getU_content())
             .usuccessedchallenge(u.getU_successedchallenge())
             .build();
-      
+
       try {
          userRepository.save(user);
          return true;
@@ -38,32 +42,34 @@ public class UserService {
          return false;
       }
    }
-   
+
    //유저 정보 목록
    public UserDTO ReadUser(String uid){
-	  
+
       UserDTO userinfo = null;
       Optional<User> result = userRepository.findById(uid);
-    	
+
       if(result.isPresent()) {
-    	  	userinfo = UserDTO.builder()
-    	  	.u_id(result.get().getUid())
-			.u_nickname(result.get().getUnickname())
-			.u_zepid(result.get().getUzepid())
-			.u_deposit(result.get().getUdeposit())
-			.u_grade(result.get().getUgrade())
-			.u_level(result.get().getUlevel())
-			.u_content(result.get().getUcontent())
-			.u_successedchallenge(result.get().getUsuccessedchallenge())
-			.build();
-    	  	return userinfo;
+          userinfo = UserDTO.builder()
+         .u_id(result.get().getUid())
+         .u_nickname(result.get().getUnickname())
+         .u_zepid(result.get().getUzepid())
+
+
+         .u_deposit(result.get().getUdeposit())
+         .u_grade(result.get().getUgrade())
+         .u_level(result.get().getUlevel())
+         .u_content(result.get().getUcontent())
+         .u_successedchallenge(result.get().getUsuccessedchallenge())
+         .build();
+            return userinfo;
       } else {
-    	  return null;
+         return null;
       }
-	     
+
    }
 
-   
+
    //유저 정보 수정
    public boolean UpdateUser(UserDTO u) {
       user = User.builder()
@@ -84,7 +90,7 @@ public class UserService {
          return false;
       }
    }
-   
+
    //유저 정보 삭제
    public boolean DeleteUser(String u_id) {
       try {
@@ -95,4 +101,42 @@ public class UserService {
          return false;
       }
    }
+
+   public void AddDeposit(String uid, int newDeposit) {
+	   userRepository.addDeposit(uid, newDeposit);
+   }
+
+   public Optional<User> SearchId(String uid) {
+	   Optional<User> userEntity = userRepository.findById(uid);
+	   return userEntity;
+   }
+
+   public String FindZepidByuID(String uid) {
+	   return userRepository.findUzepidByUid(uid);
+   }
+   
+   public int VerifyZepid(ZepIdVerifyDTO m,String uid) {
+	   String currentVerifykey = userRepository.findUzepidByUid(uid);
+	   System.out.println(m.getVerifykey() + "들어왔나?");
+	   System.out.println(currentVerifykey);
+	   if(m.getVerifykey().equals(currentVerifykey)) {
+		      try {
+		    	 userRepository.updateZepid(uid, m.getZepid());
+		    	 System.out.println("인증 성공");
+		         return 1;
+		        
+		      }catch(Exception e) {
+		         e.printStackTrace();
+		         System.out.println("이미 인증된 계정입니다.");
+		         return 2;
+		      }
+		   }else {
+			   System.out.println("인증 코드가 잘못되었습니다.");
+			   return 3;
+		   }
+
+   }
+
+
 }
+
