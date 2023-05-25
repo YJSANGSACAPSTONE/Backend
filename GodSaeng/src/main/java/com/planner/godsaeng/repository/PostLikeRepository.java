bef1c6b1f59dd2205000fad56170000cb155ea83
NoTaeGwon <tg09260926@gmail.com>
2023-05-25 10:53:33 +0900

    FIX: 게시글 전체 조회 버그 수정

--- GodSaeng/src/main/java/com/planner/godsaeng/repository/PostLikeRepository.java
+++ GodSaeng/src/main/java/com/planner/godsaeng/repository/PostLikeRepository.java
@@ -7,7 +7,18 @@ import com.planner.godsaeng.entity.User;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
 	Optional<PostLike> findByPostAndUser(Post post, User user);
+	
+    @Modifying //insert,update,delete 쿼리에서 벌크 연산시 사용한다
+    @Query("delete from PostLike pl where pl.user = :user")
+    void deleteByUser(@Param("user") User user);
+    
+    @Modifying
+    @Query("DELETE FROM PostLike pl WHERE pl.post.poid =:poid")
+    void deleteByPoid(Long poid);
 }
