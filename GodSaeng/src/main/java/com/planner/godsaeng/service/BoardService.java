9fedfa8476194f0066130e50e01bf586180df858
NoTaeGwon <tg09260926@gmail.com>
2023-06-19 11:34:55 +0900

    FIX: Board 코드 수정(게시판 생성, 삭제)

--- GodSaeng/src/main/java/com/planner/godsaeng/service/BoardService.java
+++ GodSaeng/src/main/java/com/planner/godsaeng/service/BoardService.java
@@ -1,68 +1,39 @@
 package com.planner.godsaeng.service;
 
-import java.util.ArrayList;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import com.planner.godsaeng.dto.BoardDTO;
 import com.planner.godsaeng.entity.Board;
-import com.planner.godsaeng.repository.BoardRepository;
-
-import lombok.RequiredArgsConstructor;
+import com.planner.godsaeng.entity.User;
 
 public interface BoardService {
+	
+	// 게시판 생성
+	int register(BoardDTO boardDTO);
+	
+	// 게시판 삭제
+	void removeWithAll(int poid);
+	
+	default BoardDTO entityToDto(Board board) {
+		BoardDTO boardDTO = BoardDTO.builder()
+				.u_id(board.getUser().getUid())
+				.b_id(board.getBid())
+				.b_name(board.getBname())
+				.build();
+		
+		return boardDTO;
+	}
+	
+	default Board dtoToEntity(BoardDTO boardDTO) {
+		User user = User.builder()
+				.uid(boardDTO.getU_id()).build();
+		
+		Board board = Board.builder()
+				.user(user)
+				.bid(boardDTO.getB_id())
+				.bname(boardDTO.getB_name())
+				.build();
+		
+		return board;
+		
+	}
       
-      // 게시판 작성 (Insert)
-//      public boolean InsertBoard(BoardDTO b) {
-//         board = Board.builder()
-//               .bid(b.getB_id())      // 게시판 id
-//               .aid(b.getA_id())      // 관리자 id
-//               .bname(b.getB_name())   // 게시판 이름
-//               .build();
-//         try {
-//            boardRepository.save(board);
-//            return true;
-//         }catch(Exception e) {
-//            e.printStackTrace();
-//            return false;
-//         }
-//      }
-//      
-//      // 게시판 조회 (Read)
-//      public List<Board> ReadBoard(){
-//         List<Board> list = new ArrayList<Board>();
-//         list = boardRepository.findAll();
-//         return list;
-//      }
-//      
-//      // 게시판 수정 (Update)
-//      public boolean UpdateBoard(BoardDTO b) {
-//         board = Board.builder()
-//               .bid(b.getB_id())      // 게시판 id
-//               .aid(b.getA_id())      // 관리자 id
-//               .bname(b.getB_name())   // 게시판 이름
-//               .build();
-//         try {
-//            boardRepository.save(board);
-//            return true;
-//         }catch(Exception e) {
-//            e.printStackTrace();
-//            return false;
-//         }
-//      }
-//      
-//      // 게시판 삭제 (Delete)
-//      public boolean DeleteBoard(long b_id) {
-//         try {
-//            boardRepository.deleteById(b_id);
-//            return true;
-//         }catch(Exception e) {
-//            e.printStackTrace();
-//            return false;
-//         }
-//      }
-      
-
 }
